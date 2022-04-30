e2daf0a4a463095dabdc308e50bab145698b0dfb
Vadym <vadim.barylo@intapp.com>
2022-04-30 15:38:50 +0300

    adding sonar

--- application-backend/src/test/java/com/demo/monitoring/DemoApplicationTests.java
+++ application-backend/src/test/java/com/demo/monitoring/DemoApplicationTests.java
@@ -1,12 +1,10 @@
 package com.demo.monitoring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
 class DemoApplicationTests {
     DemoApplication application = new DemoApplication();
 
